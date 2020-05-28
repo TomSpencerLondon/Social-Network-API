2b26af131365ab34e60b75e5d838fa0ece61cd11
Tom <thomas.spencer@codurance.com>
2020-05-28 09:49:07 +0100

    Add exceptions for controller

--- src/main/java/com/codurance/socialnetworkapi/SocialNetworkAPIController.java
+++ src/main/java/com/codurance/socialnetworkapi/SocialNetworkAPIController.java
@@ -9,11 +9,22 @@ import com.codurance.social_network.domain.services.FollowSubscriptionService;
 import com.codurance.social_network.domain.services.PostService;
 import com.codurance.social_network.domain.services.SocialNetworkAPI;
 import java.util.List;
+import java.util.stream.Collectors;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Validated
 public class SocialNetworkAPIController {
 
   private SocialNetworkAPI socialNetworkAPI;
@@ -29,24 +40,22 @@ public class SocialNetworkAPIController {
   }
 
   @RequestMapping("/post")
-  public void post(String name, String message) {
+  public void post(@RequestParam("name") @NotBlank String name, @RequestParam("message") @NotBlank String message) {
     socialNetworkAPI.createPost(name, message);
   }
 
   @RequestMapping(path = "/read", produces = "application/json; charset=UTF-8")
-  @ResponseBody
-  public List<Post> read(String name) {
+  public List<Post> read(@RequestParam("name") @NotBlank String name) {
     return socialNetworkAPI.getPostsFor(name);
   }
 
   @RequestMapping(path = "/wall", produces = "application/json; charset=UTF-8")
-  public List<Post> wall(String name) {
-
+  public List<Post> wall(@RequestParam("name") @NotBlank String name) {
     return socialNetworkAPI.getWallPostsFor(name);
   }
 
   @RequestMapping(path = "/follow", produces = "application/json; charset=UTF-8")
-  public void follow(String follower, String followed){
+  public void follow(@RequestParam("follower") @NotBlank String follower, @RequestParam("followed") @NotBlank String followed){
     socialNetworkAPI.save(follower, followed);
   }
 }
